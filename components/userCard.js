f990fd3cebe7e325a7e538dc31bdc8e3f4e57164
Pascall De Creator <pascall.de.creator@gmail.com>
2023-09-16 08:00:36 +0000

    Add User Card Component

---
+++ components/userCard.js
@@ -0,0 +1,14 @@
+export default function UserCard() {
+    return (
+        <div className="p-2 rounded-xl">
+            <div className="aspect-video bg-primary-dark rounded-lg"></div>
+            <div className="flex justify-between mt-2 items-start gap-2">
+                <div>
+                    <p>Lorem ipsum dolor sit.</p>
+                    <p className="text-sm text-secondary-light">Lorem Ipsum</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
