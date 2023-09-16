294660489529cd5328529dc644dac3a2a3f2ff7d
Pascall De Creator <pascall.de.creator@gmail.com>
2023-09-16 08:01:59 +0000

    Modify Pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "primary-light": "#FFFFFF",
+                "secondary-light": "#D7D7D7",
+                "accent-light": "#007BFF",
+                "cta-light": "#FF5733",
+                "link-light": "#0066CC",
+                "positive-action-light": "#00A74A",
+                "negative-action-light": "#FF3D49",
+                "warining-light": "#FFC107",
+                
+                "primary-dark": "#121212",
+                "secondary-dark": "#454545",
+                "accent-dark": "#007BFF",
+                "cta-dark": "#FF5733",
+                "link-dark": "#2196F3",
+                "positive-action-dark": "#4CAF50",
+                "negative-action-dark": "#F44336",
+                "warining-dark": "#FFC107",
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 }
